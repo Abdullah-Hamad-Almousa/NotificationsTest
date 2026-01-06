8e8ad2b981651a15f19e77ac90f6cd7d78945e58
Abdullah almousa <abdullahmlwork@gmail.com>
2026-01-07 02:16:44 +0300

    Fix issues with notification on android

--- composeApp/src/commonMain/kotlin/com/mysticbyte/notificationstest/App.kt
+++ composeApp/src/commonMain/kotlin/com/mysticbyte/notificationstest/App.kt
@@ -1,10 +1,42 @@
 package com.mysticbyte.notificationstest
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.mysticbyte.notificationstest.notification.getNotificationManager
 
 @Composable
 fun App() {
 
+    val notificationManager = remember { getNotificationManager() }
 
+    MaterialTheme {
+
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+
+            Button( onClick = {
+
+                notificationManager.requestPermission()
+
+                notificationManager.showNotification(
+                    title = "KMP Aleart",
+                    message = "This works on Android, iOS, and JVM!"
+                )
+
+            }
+
+            ){
+                Text("Show notification")
+            }
+
+        }
+
+    }
 
 }
--- composeApp/src/commonMain/kotlin/com/mysticbyte/notificationstest/vmodel/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/mysticbyte/notificationstest/vmodel/MainViewModel.kt
@@ -1,4 +1,17 @@
 package com.mysticbyte.notificationstest.vmodel
 
+import com.mysticbyte.notificationstest.notification.getNotificationManager
+
 class MainViewModel {
+
+    private val notificationManager = getNotificationManager()
+
+    fun onNotifyClicked(){
+
+        notificationManager.requestPermission()
+        notificationManager
+            .showNotification("Hello KMP!", "This works on Android, iOS, and JVM!")
+
+    }
+
 }
